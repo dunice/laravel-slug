c17f131ae44b5110ff22872443a11f573e42aa1d
dunice <dunice@gmail.com>
2020-12-17 17:53:19 -0300

    First commit

---
+++ src/SluggableServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Dunice\Sluggable;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+
+class SluggableServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+}
